9d5137d3a085794d3e3a7bce23c07eb4a274e2da
Jon DeJong <jon.dejong@gmail.com>
2016-05-06 11:37:45 -0600

    initial... nothing works yet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { Angular2MaterialExamplesAppComponent, environment } from './app/';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {HAMMER_GESTURE_CONFIG} from '@angular/platform-browser';
+import {DemoApp} from './demo-app/demo-app';
+import {HTTP_PROVIDERS} from '@angular/http';
+import {ROUTER_PROVIDERS} from '@angular/router';
+import {MdIconRegistry} from './components/icon/icon-registry';
+import {OVERLAY_CONTAINER_TOKEN} from './core/overlay/overlay';
+import {MdLiveAnnouncer} from './core/live-announcer/live-announcer';
+import {provide} from '@angular/core';
+import {createOverlayContainer} from './core/overlay/overlay-container';
+import {Renderer} from '@angular/core';
+import {MdGestureConfig} from './core/gestures/MdGestureConfig';
+import 'rxjs/Rx';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrap(Angular2MaterialExamplesAppComponent);
+bootstrap(DemoApp, [
+  ROUTER_PROVIDERS,
+  MdLiveAnnouncer,
+  provide(OVERLAY_CONTAINER_TOKEN, {useValue: createOverlayContainer()}),
+  HTTP_PROVIDERS,
+  MdIconRegistry,
+  Renderer,
+  provide(HAMMER_GESTURE_CONFIG, {useClass: MdGestureConfig})
+]);
--- src/system-config.ts
+++ src/system-config.ts
@@ -2,53 +2,84 @@
  * User Configuration.
  **********************************************************************************************/
 /** Map relative paths to URLs. */
-const map: any = {
-};
+const map:any = {};
 
 /** User packages configuration. */
-const packages: any = {
+const packages:any = {
+    'demo-app': {
+        format: 'cjs',
+        defaultExtension: 'js'
+    },
+    'components': {
+        format: 'cjs',
+        defaultExtension: 'js'
+    },
+    'core': {
+        format: 'cjs',
+        defaultExtension: 'js',
+    },
+    '@angular2-material/core': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'core.js'
+    },
+    '@angular2-material/toolbar': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'toolbar.js'
+    },
+    '@angular2-material/icon': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'icon.js'
+    },
+    '@angular2-material/button': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'button.js'
+    }
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
  * Everything underneath this line is managed by the CLI.
  **********************************************************************************************/
-const barrels: string[] = [
-  // Angular specific barrels.
-  '@angular/core',
-  '@angular/common',
-  '@angular/compiler',
-  '@angular/http',
-  '@angular/router',
-  '@angular/platform-browser',
-  '@angular/platform-browser-dynamic',
+const barrels:string[] = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
 
-  // Thirdparty barrels.
-  'rxjs',
+    // Thirdparty barrels.
+    'rxjs',
 
-  // App specific barrels.
-  'app',
-  'app/shared',
-  /** @cli-barrel */
+    // App specific barrels.
+    'app',
+    'app/shared',
+    /** @cli-barrel */
 ];
 
-const cliSystemConfigPackages: any = {};
-barrels.forEach((barrelName: string) => {
-  cliSystemConfigPackages[barrelName] = { main: 'index' };
+const _cliSystemConfig = {};
+barrels.forEach((barrelName:string) => {
+    (<any> _cliSystemConfig)[barrelName] = {main: 'index'};
 });
 
 /** Type declaration for ambient System. */
-declare var System: any;
+declare var System:any;
 
 // Apply the CLI SystemJS configuration.
 System.config({
-  map: {
-    '@angular': 'vendor/@angular',
-    'rxjs': 'vendor/rxjs',
-    'main': 'main.js'
-  },
-  packages: cliSystemConfigPackages
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: _cliSystemConfig
 });
 
 // Apply the user's configuration.
-System.config({ map, packages });
+System.config({map, packages});
